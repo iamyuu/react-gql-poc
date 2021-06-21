d0eb491cfca8bab3adcfc74dd30e4f8bf19d32ab
Yusuf <hello@iamyuu.dev>
2021-06-21 23:07:50 +0700

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Layout from './components/layout'
 
+import NotFoundScreen from './screens/not-found'
+import DashboardScreen from './screens/dashboard'
+import ProfileScreen from './screens/profile'
+import EmployeeListScreen from './screens/employee/list'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<DashboardScreen />} />
+      <Route path='/profile' element={<ProfileScreen />} />
+      <Route path='/employee/list' element={<EmployeeListScreen />} />
+      <Route path='*' element={<NotFoundScreen />} />
+    </Routes>
+  )
+}
+
 export default function App() {
-  return <Layout>content</Layout>
+  return (
+    <Router>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </Router>
+  )
 }
