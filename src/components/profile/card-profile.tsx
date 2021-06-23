9a70bdaba1fec78f6d01a56b6163c48f48df5411
Yusuf <hello@iamyuu.dev>
2021-06-23 16:51:46 +0700

    feat: add error boudary for card profile

--- src/components/profile/card-profile.tsx
+++ src/components/profile/card-profile.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import {Card, Typography, Skeleton} from 'antd'
+import {Card, Typography, Skeleton, Alert, Button} from 'antd'
+// import ErrorBoundary from '~/components/error-boundary'
+import type {FallbackProps} from 'react-error-boundary'
 import {useProfileQuery} from '~/generated/graphql'
 
 export function CardProfileLoading() {
@@ -13,7 +15,22 @@ export function CardProfileLoading() {
   )
 }
 
-export default function CardProfile() {
+export function CardProfileFallbackError(props: FallbackProps) {
+  return (
+    <Alert
+      type='error'
+      message='There was an error!'
+      description={props.error.message}
+      action={
+        <Button size='small' type='default' onClick={() => props.resetErrorBoundary()}>
+          Try again
+        </Button>
+      }
+    ></Alert>
+  )
+}
+
+export default function CardProfileDataView() {
   const {data, error, isError} = useProfileQuery({}, {suspense: true})
 
   if (isError) {
@@ -27,3 +44,13 @@ export default function CardProfile() {
     </Card>
   )
 }
+
+// export default function CardProfile() {
+//   return (
+//     <ErrorBoundary FallbackComponent={CardProfileFallbackError}>
+//       <React.Suspense fallback={<CardProfileLoading />}>
+//         <CardProfileDataView />
+//       </React.Suspense>
+//     </ErrorBoundary>
+//   )
+// }
--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {Row, Col} from 'antd'
-import {CardProfileLoading} from '~/components/profile/card-profile'
+import ErrorBoundary from '~/components/error-boundary'
+import {CardProfileLoading, CardProfileFallbackError} from '~/components/profile/card-profile'
 
 const CardProfile = React.lazy(() => import('~/components/profile/card-profile'))
 
@@ -8,9 +9,11 @@ export default function DashboardScreen() {
   return (
     <Row gutter={[8, 8]}>
       <Col span={8}>
-        <React.Suspense fallback={<CardProfileLoading />}>
-          <CardProfile />
-        </React.Suspense>
+        <ErrorBoundary FallbackComponent={CardProfileFallbackError}>
+          <React.Suspense fallback={<CardProfileLoading />}>
+            <CardProfile />
+          </React.Suspense>
+        </ErrorBoundary>
       </Col>
       <Col span={16}>TODO: add todo</Col>
     </Row>
